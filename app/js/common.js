0fa476ec19c9fa1d9f0fc00457333aa9058993a4
Savitskiy Dmitriy <sava2503@gmail.com>
2016-07-21 11:25:37 +0300

    добавлен календарь на страницу финансов. Страница закрыта

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,35 @@
 $(function() {
 
 ///скрипты страницы финансов/////
+var dateFormat = "mm/dd/yy",
+		from = $( "#from" )
+			.datepicker({
+				defaultDate: "+1w",
+				changeMonth: true,
+			})
+			.on( "change", function() {
+				to.datepicker( "option", "minDate", getDate( this ) );
+			}),
+		to = $( "#to" ).datepicker({
+			defaultDate: "+1w",
+			changeMonth: true,
+		})
+		.on( "change", function() {
+			from.datepicker( "option", "maxDate", getDate( this ) );
+		});
+
+	function getDate( element ) {
+		var date;
+		try {
+			date = $.datepicker.parseDate( dateFormat, element.value );
+		} catch( error ) {
+			date = null;
+		}
+
+		return date;
+	}
+
+
    function financesRemoveAnim(){
      $(this).children('i').removeClass('finReloadd');
     }
